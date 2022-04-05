52585d387005c3bd96e66b3f020e0f29a8bbda9c
Hariprasath G <hariprasath167@gmail.com>
2022-04-05 12:42:37 +0530

    Time_Conversion_Problem

--- HackerRank-Algorithms-warmup-Time_Conversion.py
+++ HackerRank-Algorithms-warmup-Time_Conversion.py
@@ -11,13 +11,15 @@ def timeConversion(s):
     # We can't say number of inputs in 'AM' is higher/lower or number of inputs in 'PM' is higher/lower, it varies on time to time.
  
     # 1. From "12:00:00AM" to "12:59:59AM" - period of 1 hour
-    # 2. From "01:00:00AM" to "11:59:59AM" - period of 11 hour
-    # 3. From "12:00:00PM" to "12:59:59PM" - period of 1 hour
-    # 4. From "01:00:00PM" to "11:59:59PM" - period of 11 hour
+    # 2. From "01:00:00AM" to "12:59:59PM" - period of 12 hour
+    # 3. From "01:00:00PM" to "11:59:59PM" - period of 11 hour
 
     # Now, we can easily prioritize conditions based upon the higher number of time period
 
-    if meridian == 'PM' and hour < 12: # satisfies 4th point and this is most possiblility
+    if (meridian == 'AM' and hour != 12) or (meridian == 'PM' and hour == 12): # satisfies 2nd point and this is most possibility of 12 hours time period
+        print("".join(s[: -2]))
+    
+    elif meridian == 'PM' and hour < 12: # satisfies 4th point and this is most possiblility of 11 hours time period
 
         # check hour is less than 12. If so, add the hour value to 12 and just replace hour value in the postion s[0] and s[1] 
         hour += 12
@@ -26,18 +28,10 @@ def timeConversion(s):
         # join function in list is used here to join each character to form a string and neglect s[8] and s[9] i.e., meridian value
         print("".join(s[: -2]))
 
-    elif meridian == 'AM' and hour != 12: # satisfies 2nd point and this is most possibility 
-        print("".join(s[: -2]))
-    
-    elif meridian == 'PM' and hour == 12: # satisfies 3rd point and this is least possibility
-        print("".join(s[: -2]))
-
     elif meridian == 'AM' and hour == 12: # satisfies 1st point and this is least possibility
         s[0], s[1] = '0', '0'
         print("".join(s[: -2]))
         
-    
-
 if __name__ == '__main__':
     # In the first of console get time as a string and store it in variable 's'  
     s = input()
